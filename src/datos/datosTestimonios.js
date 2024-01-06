22aa8b5ea6f4ac9e075b051f5a3f263a792c89d0
Denys <99731617+Leid04@users.noreply.github.com>
2024-01-06 13:49:42 +0100

    Layout en React

---
+++ src/datos/datosTestimonios.js
@@ -0,0 +1,19 @@
+const datosTestimonios = [
+    {
+      nombre: 'Arbol',
+      pais: 'Africa',
+      imagen: 'imagen1',
+      cargo: 'Dar el oxígeno',
+      empresa: 'Mundo',
+      testimonio: 'Hola, doy oxígeno para ti',
+    },
+    {
+      nombre: 'Tierra',
+      pais: 'Universo',
+      imagen: 'imagen2',
+      cargo: 'Soporta humanos',
+      empresa: 'Sistema solar',
+      testimonio: 'El lugar que alberga la vida',
+    },
+  ];
+export default datosTestimonios;
